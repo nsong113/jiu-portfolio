dc32cc47b28c2edace5f6e10aa5506541b171129
nsong113 jiu <nsong113@gmail.com>
2024-05-26 04:59:43 +0900

    feat: about 완성

--- src/components/about/AboutContent.tsx
+++ src/components/about/AboutContent.tsx
@@ -1,7 +1,28 @@
-import React from "react";
-
-const AboutContent = () => {
-  return <div>AboutContent</div>;
-};
-
-export default AboutContent;
+export default function AboutContent() {
+  return (
+    <div className="mb-4 mt-6 tracking-wide flex flex-col gap-1 text-stone-600">
+      <p className="text-sm sm:text-base lg:text-lg md:text-lg ">
+        안녕하세요! 프론트엔드 개발자 송지우입니다.
+      </p>
+      <p className="text-sm sm:text-base mt-2 lg:text-lg md:text-lg ">
+        제가 하는 일에서 항상 즐거움을 찾으려고 합니다. 뭐든 즐기면서 하면
+        결과도 따라온다고 믿어요.
+      </p>
+      <p className="text-sm sm:text-base lg:text-lg md:text-lg ">
+        함께 일하는 동료에게도, 같이 일하면 즐거운 사람이고 싶어요.
+      </p>
+      <p className="text-sm sm:text-base lg:text-lg md:text-lg">
+        나와 동료와의 능률을 올리는 깔끔한 코드도, 사용자를 즐겁게 할 수 있는
+        UX도 고려하며 작업합니다.
+      </p>
+      <p className="text-sm sm:text-base mt-4 lg:text-lg md:text-lg ">
+        사람들을 만나는 걸 좋아해서, 다양한 직군과 함께 좋은 결과물을 만드는 데
+        관심이 많아요.
+      </p>
+      <p className="text-sm sm:text-base lg:text-lg md:text-lg ">
+        모두가 만족할 수 있는 협업을 목표로 개선할 점을 고민하고 항상 적극적으로
+        커뮤니케이션합니다.
+      </p>
+    </div>
+  );
+}
--- src/components/common/ScrollDown.tsx
+++ src/components/common/ScrollDown.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+"use client";
 
-const ScrollDown = () => {
-  return <div>ScrollDown</div>;
-};
+import Lottie from "react-lottie-player";
+import scrollDown from "../../../public/images/lottie/scroll-down.json";
 
-export default ScrollDown;
+export default function ScrollDown() {
+  return (
+    <div className="w-7 h-7 md:w-9 md:h-9 mx-auto pulse-animation z-20">
+      <div className="w-10 h-10 md:w-14 md:h-14">
+        <Lottie animationData={scrollDown} loop play />
+      </div>
+    </div>
+  );
+}
